15a8983407e83f5e34c86f3bd577caadd87f59c0
Mounika-reddy-mukku <mounikareddtmukku@gmail.com>
2023-10-05 09:49:54 +0530

    git push

---
+++ datatypes/type.js
@@ -0,0 +1,14 @@
+var price=1000;
+var rating=4.5;
+var brand='roadstar'
+var colour=['blue','black','red','white','pink']
+var size={size1:28, size2:30}
+var a=true
+var b;
+console.log(typeof price)
+console.log(typeof rating)
+console.log(typeof brand)
+console.log(typeof colour)
+console.log(typeof size)
+console.log(typeof a)
+console.log(typeof b)
